98c392dbdc7cb056dacb6c6c31211b7725285f77
Your Name <you@example.com>
2025-05-23 18:48:45 +0200

    cwiczenie 1

--- cwiczenie1/index.php
+++ cwiczenie1/index.php
@@ -2,33 +2,26 @@
 <html lang="pl">
 
 <head>
-
 	<meta charset="utf-8">
 	<title>1 - Obsługa formularzy w PHP</title>
 	<link href="arkusz-cwiczenia.css" rel="stylesheet">
-	
 </head>
 
 <body>
 
 	<main>
-	
 		<article>
-
 			<header>
-			
 				<h1>Tokeny bezpieczeństwa SAFE - logowanie</h1>
-
 			</header>
-	
-			<form action="index.php" method="post"> 
-			
-				<div>	
+
+			<form action="index.php" method="post">
+				<div>
 					<label>Login:
 						<input type="text" name="login">
 					</label>
-				</div>	
-				
+				</div>
+
 				<div>
 					<label>Hasło:
 						<input type="password" name="haslo">
@@ -36,27 +29,34 @@
 				</div>
 
 				<input type="submit" value="Zaloguj">
-				
 			</form>
-<?php
 
-function token($dlugosc = 10) {
-    $znaki = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $znakiDlugosc = strlen($znaki);
-    $randomString = '';
-    for ($i = 0; $i < $dlugosc; $i++) {
-        $randomString .= $znaki[rand(0, $znakiDlugosc - 1)];
-    }
-    return $randomString;
-}
+			<?php
+			function token($dlugosc = 10) {
+				$znaki = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+				$znakiDlugosc = strlen($znaki);
+				$randomString = '';
+				for ($i = 0; $i < $dlugosc; $i++) {
+					$randomString .= $znaki[rand(0, $znakiDlugosc - 1)];
+				}
+				return $randomString;
+			}
 
-echo "<p>Jednorazowy token dostępu do skarbca: " . token(10) . "</p>";
+			if (isset($_POST['haslo']) && isset($_POST['login'])) {
+				$haslo = $_POST['haslo'];
+				$login = $_POST['login'];
 
-?>
-
+				if ($login == "pawlo" && $haslo == "napadnabank") {
+					echo "Witaj Paweł, weź się do roboty leniu xD <br> Jednorazowy kod dostępu do skarbca: " . token();
+				} else if ($login == "gawlo" && $haslo == "likeaboss") {
+					echo "Witaj Gaweł, szefie wszystkich szefów! <br> Jednorazowy kod dostępu do skarbca: " . token();
+				} else {
+					echo "Niepoprawny login lub hasło";
+				}
+			}
+			?>
 		</article>
-		
 	</main>
-	
+
 </body>
 </html>
